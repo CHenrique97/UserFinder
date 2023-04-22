f5922b7a92ed5ea50118238a0c420bccbabbaabb
Carlos Custodio <chcustodio97@gmail.com>
2023-04-22 11:00:46 -0300

    growing pain

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/UserFinder/controllers"
 	"github.com/UserFinder/initializers"
 	"github.com/UserFinder/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,12 +21,15 @@ func main() {
 
 	r := gin.Default()
 
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"} // Replace with your client's URL
+	r.Use(cors.New(config))
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello World")
 	})
 
 	r.GET("/migrate", controllers.Migrate)
-	r.GET("/getUser", controllers.GetUser)
+	r.POST("/getUser", controllers.GetUser)
 	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 	r.POST("/postUser", controllers.PostCreate)
 	r.Run(":" + os.Getenv("port"))
